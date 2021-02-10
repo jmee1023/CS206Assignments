720344ffb6391a56eeef059568f0b9e80eb7887c
jmee1023 <jmmee@uvm.edu>
2021-02-10 12:28:24 -0500

    Pyramid implemented

--- generate.py
+++ generate.py
@@ -1,8 +1,36 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("box.sdf")
+pyrosim.Start_SDF("boxes.sdf")
+length = 1
+width = 1
+height = 1
+x = 0
+y = 0
+z = .5
+for count in range(5):
+	length = 1
+	width = 1
+	height = 1
+	y = 0
+	x+=1
+	z = 0
+	pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
+	for count1 in range(5):
+		length = 1
+		width = 1
+		height = 1
+		z = 0
+		y +=1
+		pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
+		for count3 in range(5):
+			z +=1
+			#pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
+			length *= .9
+			width *= .9
+			height *= .9		
+			pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
 
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[1,1,1])
-
+#pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
+#pyrosim.Send_Cube(name="Box2", pos=[x,y+1,z+1] , size=[length,width,height])
 pyrosim.End()
 
