e3eeec7346cacfbcb5670890e463e66390e35afc
jmee1023 <jmmee@uvm.edu>
2021-02-10 13:35:46 -0500

    World and Robot implemented

--- generate.py
+++ generate.py
@@ -1,36 +1,27 @@
 import pyrosim.pyrosim as pyrosim
+def Create_World():
+        pyrosim.Start_SDF("world.sdf")
+        pyrosim.Send_Cube(name="Box", pos=[2,0,.5] , size=[length,width,height])
+        pyrosim.End()
 
-pyrosim.Start_SDF("boxes.sdf")
+def Create_Robot():
+	pyrosim.Start_URDF("body.urdf")
+	pyrosim.Send_Cube(name="Torso", pos=[x,y,z] , size=[length,width,height])
+	pyrosim.End()
+
+
+
+#pyrosim.Start_SDF("world.sdf")
 length = 1
 width = 1
 height = 1
 x = 0
 y = 0
 z = .5
-for count in range(5):
-	length = 1
-	width = 1
-	height = 1
-	y = 0
-	x+=1
-	z = 0
-	pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
-	for count1 in range(5):
-		length = 1
-		width = 1
-		height = 1
-		z = 0
-		y +=1
-		pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
-		for count3 in range(5):
-			z +=1
-			#pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
-			length *= .9
-			width *= .9
-			height *= .9		
-			pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
-
 #pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
-#pyrosim.Send_Cube(name="Box2", pos=[x,y+1,z+1] , size=[length,width,height])
-pyrosim.End()
+Create_World()
+Create_Robot()
+
+#pyrosim.End()
+
 
