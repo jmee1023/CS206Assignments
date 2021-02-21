be957972a8f74a3499f3f4e09be639fb0dc1f08b
jmee1023 <jmmee@uvm.edu>
2021-02-21 10:12:03 -0500

    Sensor Visualization

--- analyze.py
+++ analyze.py
@@ -1,1 +1,16 @@
-include numpy
+import matplotlib.pyplot
+import numpy
+
+
+backLegSensorValues = numpy.load('data/backLegSensorValues.npy')
+frontLegSensorValues = numpy.load('data/frontSensorValues.npy')
+
+matplotlib.pyplot.plot(backLegSensorValues, label = "Back Leg", linewidth = 8)
+#matplotlib.pyplot.legend("Back Leg Value")
+matplotlib.pyplot.plot(frontLegSensorValues, label = "Front Leg")
+
+
+#print(frontLegSensorValues)
+#print(backLegSensorValues)
+matplotlib.pyplot.legend()
+matplotlib.pyplot.show()
