71be9a75081daa238ea521ae6ed89f294234a66c
jmee1023 <jmmee@uvm.edu>
2021-02-21 10:12:53 -0500

    Sensor Visualization

--- simulate.py
+++ simulate.py
@@ -3,6 +3,8 @@ import time
 import pybullet as p
 import pyrosim.pyrosim as pyrosim
 import numpy
+import os
+
 
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
@@ -13,14 +15,18 @@ p.loadSDF("world.sdf")
 pyrosim.Prepare_To_Simulate("body.urdf")
 
 backLegSensorValues = numpy.zeros(10000)
-
+frontLegSensorValues = numpy.zeros(10000)
 
 for i in range(1000):
 	p.stepSimulation()
 	backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
+	frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
+
 	#backLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
 	time.sleep(1/60)
 
-print(backLegSensorValues)
+numpy.save('data/sensorValues.npy',backLegSensorValues)
+numpy.save('data/frontSensorValues.npy',frontLegSensorValues)
+print(frontLegSensorValues)
 p.disconnect() 
 
