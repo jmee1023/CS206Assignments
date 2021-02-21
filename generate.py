9066f33a86b7ea99fe08befd690c9f47dcf6055f
jmee1023 <jmmee@uvm.edu>
2021-02-21 10:13:14 -0500

    Sensor Visualization

--- generate.py
+++ generate.py
@@ -1,14 +1,16 @@
 import pyrosim.pyrosim as pyrosim
 def Create_World():
         pyrosim.Start_SDF("world.sdf")
-        pyrosim.Send_Cube(name="Box", pos=[2,0,.5] , size=[length,width,height])
+        pyrosim.Send_Cube(name="Box", pos=[5,0,.5] , size=[length,width,height])
         pyrosim.End()
 
 def Create_Robot():
 	pyrosim.Start_URDF("body.urdf")
-	pyrosim.Send_Cube(name="Torso", pos=[0,0,0.5] , size=[1,1,1])
-	pyrosim.Send_Joint( name = "Torso_Leg" , parent= "Torso" , child = "Leg" , type = "revolute", position = "0.5 0 1.0")
-	pyrosim.Send_Cube(name="Leg", pos=[0.5,0,0.5] , size=[1,1,1])
+	pyrosim.Send_Cube(name="Torso", pos=[1,0,1.5] , size=[1,1,1])
+	pyrosim.Send_Joint( name = "Torso_BackLeg" , parent= "Torso" , child = "BackLeg" , type = "revolute", position = "0.5 0 1.0")
+	pyrosim.Send_Cube(name="BackLeg", pos=[-0.5,0,-0.5] , size=[1,1,1])
+	pyrosim.Send_Joint( name = "Torso_FrontLeg" , parent= "Torso" , child = "FrontLeg" , type = "revolute", position = "1.5 0 1.0")
+	pyrosim.Send_Cube(name="FrontLeg", pos=[.5,0,-.5] , size=[1,1,1])
 	pyrosim.End()
 
 
