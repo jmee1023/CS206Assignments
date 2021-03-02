75d9a0871c917eeb4d4d34a679191450eb4f84a0
jmee1023 <jmmee@uvm.edu>
2021-03-02 12:22:56 -0500

    Robot walking implemented

--- simulate.py
+++ simulate.py
@@ -5,6 +5,8 @@ import pyrosim.pyrosim as pyrosim
 import numpy
 import os
 import math
+import random
+
 
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
@@ -14,10 +16,33 @@ robot = p.loadURDF("body.urdf")
 p.loadSDF("world.sdf")
 pyrosim.Prepare_To_Simulate("body.urdf")
 
+#BackLeg Variables
+BackLegAmplitude = numpy.pi/4
+BackLegFrequency = 10
+BackLegPhaseOffset = 0
+
+#Front leg variables
+FrontLegAmplitude = numpy.pi/4
+FrontLegFrequency = 10
+FrontLegPhaseOffset = 1
 
 #Initialize Vectors
-backLegSensorValues = numpy.zeros(10000)
-frontLegSensorValues = numpy.zeros(10000)
+backLegSensorValues = numpy.zeros(1000)
+frontLegSensorValues = numpy.zeros(1000)
+
+#Vector to store values sinusioudally over the ranges -pi to pi
+#targetAngles =(numpy.pi/4) *  numpy.sin( numpy.linspace( -numpy.pi, numpy.pi , 1000))
+BackLegTargetAngles =BackLegAmplitude * numpy.sin(BackLegFrequency *  numpy.linspace( -numpy.pi, numpy.pi , 1000) + BackLegPhaseOffset)
+FrontLegTargetAngles =FrontLegAmplitude * numpy.sin(FrontLegFrequency *  numpy.linspace( -numpy.pi, numpy.pi , 1000) + FrontLegPhaseOffset)
+ 
+#print(targetAngles)
+
+#Save targetAnles values to disk
+#numpy.save('data/targetAngles.npy',targetAngles)
+#exit()
+numpy.save('data/BackLegTargetAngles.npy', BackLegTargetAngles)
+numpy.save('data/FrontLegTargetAngles.npy', FrontLegTargetAngles)
+
 
 for i in range(1000):
 	p.stepSimulation()
@@ -28,8 +53,8 @@ for i in range(1000):
 	bodyIndex = robot,
 	jointName = "Torso_BackLeg",
 	controlMode = p.POSITION_CONTROL,
-	targetPosition =-(math.pi)/4.0 ,
-	maxForce = 500)
+	targetPosition =  BackLegTargetAngles[i] ,
+	maxForce = 40)
 
 
 	#2nd motor
@@ -37,16 +62,19 @@ for i in range(1000):
         bodyIndex = robot,
         jointName = "Torso_FrontLeg",
         controlMode = p.POSITION_CONTROL,
-        targetPosition = (math.pi)/4.0 ,
-	maxForce = 500)
+        targetPosition = FrontLegTargetAngles[i] ,
+	maxForce = 40)
 
 
 
 
 	time.sleep(1/60)
 
-numpy.save('data/sensorValues.npy',backLegSensorValues)
-numpy.save('data/frontSensorValues.npy',frontLegSensorValues)
+#numpy.save('data/sensorValues.npy',backLegSensorValues)
+#numpy.save('data/frontSensorValues.npy',frontLegSensorValues)
+
+
+
 print(frontLegSensorValues)
 p.disconnect() 
 
