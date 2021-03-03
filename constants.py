05dd48a09f62546c40532efdf95ebea9ef66c515
jmee1023 <jmmee@uvm.edu>
2021-03-03 17:34:53 -0500

    Sensor vectors outputed

---
+++ constants.py
@@ -0,0 +1,22 @@
+import pyrosim.pyrosim as pyrosim
+import numpy
+import os
+import math
+import random
+
+
+#Back Leg Variables
+BlAmplitude = numpy.pi/4
+BlFreq = 10
+BlOffset = 0
+
+#Front leg variables
+FlAmplitude = numpy.pi/4
+FlFreq = 10
+FlOffset = 1
+
+#Initialize Vectors
+vectorLength = 1000
+#MAX force
+maxForce = 100
+
