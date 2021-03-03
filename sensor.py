a75deba81d4fcdbbce45f8e9928a71e452747720
jmee1023 <jmmee@uvm.edu>
2021-03-03 17:35:21 -0500

    Sensor vectors outputed

---
+++ sensor.py
@@ -0,0 +1,20 @@
+import pybullet_data
+import time
+import pybullet as p
+import pyrosim.pyrosim as pyrosim
+import numpy
+import os
+import math
+import random
+import constants as c
+
+class SENSOR:
+	def __init__(self,name):
+		self.linkName = name
+		self.values = numpy.zeros(c.vectorLength)
+		#print(self.values)
+
+	def Get_Value(self,t):
+		self.values[t] = pyrosim.Get_Touch_Sensor_Value_For_Link(self.linkName)
+		if(t == 999):
+			print(self.values)
